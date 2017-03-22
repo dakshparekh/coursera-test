24ef7fbca8772656d4fcfefc0dd6f61028532f98
dakshparekh <dakshparekh@rediffmail.com>
2017-03-22 11:49:51 +0530

    Create script.js

---
+++ m2/js/script.js
@@ -0,0 +1,40 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var temp =
+   document.getElementById("name").value;
+                 
+              if ( temp === res.student1) {
+             
+                var temp2= res.question1+": "
+                
+              }
+              else if (temp === res.student2) {
+                var temp3= res.question2+": "
+              }
+          else {
+            var temp1 = "no such student";
+          }
+          if
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + temp+ "</h2>";
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + temp2+ "</h2>";
+          document.querySelector("#content1")
+                .innerHTML = "<h2>" + temp1+ "</h2>";
+            });
+      });
+  
+  }
+);
